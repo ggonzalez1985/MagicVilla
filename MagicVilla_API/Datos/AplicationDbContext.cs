52220033b9c5205f71161e8ae69f7181d2eb2b9e
ggonzalez1985 <gabrielgonzalez1985@gmail.com>
2024-09-23 18:45:39 -0300

    EF + Cad. de conexion ya OK

--- MagicVilla_API/Datos/AplicationDbContext.cs
+++ MagicVilla_API/Datos/AplicationDbContext.cs
@@ -5,6 +5,12 @@ namespace MagicVilla_API.Datos
 {
     public class AplicationDbContext :DbContext
     {
+        //con esto toma la connection string que especifique en "Program.cs"
+        public AplicationDbContext(DbContextOptions<AplicationDbContext> options) :base(options)
+        {
+                
+        }
+
         public DbSet<Villa> Villas { get; set; }
     }
 }
